48ed9a326fe11678d3534211ac4ec22abd431951
Francisco <victoriafrancis2009@gmail.com>
2021-01-10 20:52:24 -0300

    Aplicación semana 2

---
+++ cajero.py
@@ -0,0 +1,11 @@
+"""
+Cree una aplicación que pida al usuario ingresar un monto a pagar. Luego su programa le pide al usuario ingresar con cuánto efectivo va a realizar el pago. Calcular la cantidad de modenas ($10, $50, $100, $500) y billetes ($1000, $2000, $5000, $10000, $20000) con los que se le dará el vuelto y muestre el vuelto total y el efectivo involucrado. Siga el siguiente ejemplo de ejecución:
+Ingrese cantidad a pagar $: 4550
+Ingrese efectivo $: 10000
+Su vuelto es $5450 compuesto por:
+1 billete de $5000
+4 monedas de $100
+1 moneda de $50
+"""
+#cualquier supuesto dejar escrito como comentario.-
+#Puede usar todo lo visto hasta listas y tuplas.
