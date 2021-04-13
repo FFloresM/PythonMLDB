fafdefe66153e8f8e44b5058320d6146552e9fd6
Francisco <victoriafrancis2009@gmail.com>
2021-04-13 19:32:11 -0400

    formulario entrada

--- intranet/intra/urls.py
+++ intranet/intra/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'intra'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('entrada/', views.OficioEntradaCreateView.as_view(), name='formulario-entrada'),
 ]
--- intranet/intra/views.py
+++ intranet/intra/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from .models import OficioEntrada
+from django.urls import reverse, reverse_lazy
 
 @login_required()
 def index(request):
     var = "hola mundo"
     return render(request, "intra/base.html", {"hola": var})
+
+class OficioEntradaCreateView(CreateView):
+    model = OficioEntrada
+    fields = ['fecha_oficio', 'numero_oficio', 'referencia', 'observacion', 'institucion']
+    success_url = reverse_lazy('intra:index')
+    
+
     
